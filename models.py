2c5ac44cc903dd6c804c38dd28f92a71b5b2b67d
samcw <tsukihie@gmail.com>
2020-10-07 14:54:54 +0800

    add ecalayer

--- models.py
+++ models.py
@@ -4,6 +4,34 @@ from utils.parse_config import *
 
 ONNX_EXPORT = False
 
+# EA layer
+class Eca_layer(nn.Module):
+    """Constructs a ECA module.
+    Args:
+        channel: Number of channels of the input feature map
+        k_size: Adaptive selection of kernel size
+    """
+    def __init__(self, channel, k_size=3):
+        super(eca_layer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.conv = nn.Conv1d(1, 1, kernel_size=k_size, padding=(k_size - 1) // 2, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        # x: input features with shape [b, c, h, w]
+        b, c, h, w = x.size()
+
+        # feature descriptor on the global spatial information
+        y = self.avg_pool(x)
+
+        # Two different branches of ECA module
+        y = self.conv(y.squeeze(-1).transpose(-1, -2)).transpose(-1, -2).unsqueeze(-1)
+
+        # Multi-scale information fusion
+        y = self.sigmoid(y)
+
+        return x * y.expand_as(x)
+
 # GhostModule
 class GhostModule(nn.Module):
     def __init__(self, inp, oup, kernel_size=1, ratio=2, dw_size=3, stride=1):
@@ -53,6 +81,7 @@ def create_modules(module_defs, img_size, cfg):
                 #                                        groups=mdef['groups'] if 'groups' in mdef else 1,
                 #                                        bias=not bn))
                 modules.add_module('Conv2d', GhostModule(inp=output_filters[-1], oup=filters, kernel_size=k, stride=stride))
+                modules.add_module('Eca', Eca_layer(filters, k_size=k))
             else:  # multiple-size conv
                 modules.add_module('MixConv2d', MixConv2d(in_ch=output_filters[-1],
                                                           out_ch=filters,
