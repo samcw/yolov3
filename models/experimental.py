a5c511f6679f852f7d2178cf0c9673dfc8de1a70
Histo <tsukihie@gmail.com>
2020-12-10 09:04:55 +0800

    fix

--- models/experimental.py
+++ models/experimental.py
@@ -152,7 +152,7 @@ class GhostBottleneck(nn.Module):
         c_ = c2 // 2
         self.conv = nn.Sequential(GhostConv(c1, c_, 1, 1),  # pw
                                   DWConv(c_, c_, k, s, act=False) if s == 2 else nn.Identity(),  # dw
-                                  SqueezeExcite(c_),  # se
+                                  # SqueezeExcite(c_),  # se
                                   GhostConv(c_, c2, 1, 1, act=False))  # pw-linear
         self.shortcut = nn.Sequential(DWConv(c1, c1, k, s, act=False),
                                       Conv(c1, c2, 1, 1, act=False)) if s == 2 else nn.Identity()
