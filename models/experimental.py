bf3619bc857bd1674a2fdc09ae6519dfe5d7252e
Histo <tsukihie@gmail.com>
2020-12-15 20:08:27 +0800

    fix

--- models/experimental.py
+++ models/experimental.py
@@ -297,7 +297,7 @@ class GhostBottleneckSandGlass(nn.Module):
 
         # Depth-wise for more space detail
         self.dw2 = nn.Sequential(
-            nn.Conv2d(out_chs, out_chs, 3, stride=stride, padding=(dw_kernel_size - 1) // 2, groups=in_chs, bias=False),
+            nn.Conv2d(out_chs, out_chs, 3, stride=stride, padding=(dw_kernel_size - 1) // 2, groups=out_chs, bias=False),
             nn.BatchNorm2d(out_chs),
             nn.Linear(out_chs, out_chs, False)
         )
